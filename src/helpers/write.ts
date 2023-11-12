b5bdb2284157b430c9abb8a4105ad7375764cb8a
littlefatcat <littlefatcat@163.com>
2023-11-12 23:11:58 +0800

    fix(helper): witer add href of hexo url_for

--- src/helpers/write.ts
+++ src/helpers/write.ts
@@ -1,15 +1,16 @@
 import path from 'path'
 import fs from 'fs'
-import { genSvg } from '../utils/vueTsr'
 
 hexo.extend.helper.register('write_info', function (point) {
+    // @ts-ignore
+    const _this = this
     if (point === 'footer') {
         const { name, version } = JSON.parse(
             fs.readFileSync(path.join(hexo.base_dir, 'package.json'), 'utf-8')
         )
         const tmpPath = path.join(hexo.theme_dir, 'templates', 'footer.ejs')
-        const icon = genSvg('chat-left-dots')
-        return hexo.render.renderSync({ path: tmpPath }, { name, version, icon })
+        const href = _this.url_for('about')
+        return hexo.render.renderSync({ path: tmpPath }, { name, version, href })
     }
     return
 })
